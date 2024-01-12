0c34c44be3b1b9e784c0e1f4adf6f75a94d65f2d
Harsh Jain <143304594+harshjain1729@users.noreply.github.com>
2024-01-12 19:51:23 +0530

    Delete iiitdrive directory

--- iiitdrive/forms.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-class NewUserCreationForm(UserCreationForm):
-	class Meta:
-		model = User
-		fields = ("username", "email", "password1", "password2")
