e26aa2dba9234dd683c5cd70cefc5aaf6d8ee59c
Harsh Jain <143304594+harshjain1729@users.noreply.github.com>
2024-01-12 19:51:55 +0530

    Delete staticfiles directory

--- staticfiles/js/index.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('input, select').on('focus', function () {
-        $(this).parent().find('.input-group-text').css('border-color', '#80bdff');
-    });
-    $('input, select').on('blur', function () {
-        $(this).parent().find('.input-group-text').css('border-color', '#ced4da');
-    });
-});
